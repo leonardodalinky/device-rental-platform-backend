3eaf3cb75d2436d271f3041684f245868a3c52dc
leonardodalinky <493987054@qq.com>
2020-09-07 20:21:22 +0800

    First push. A very first version of framework.

---
+++ webservice/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# Register your models here.
+# TODO: 注册模型管理
---
+++ webservice/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+# TODO: 增加各个 view
+
+from .views import *
+
+urlpatterns = [
+
+]
