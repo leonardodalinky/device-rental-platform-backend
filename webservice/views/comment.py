2cda1c2aacb15e668388b08f75350251869a262f
leonardodalinky <493987054@qq.com>
2020-09-09 01:47:21 +0800

    修复common包的引用

--- webservice/views/comment.py
+++ webservice/views/comment.py
@@ -7,7 +7,7 @@ from django.http import HttpRequest, JsonResponse
 from django.db.models.query import QuerySet
 from django.views import View
 
-from ..common import create_error_json_obj, create_not_login_json_response, create_success_json_res_with
+from ..common.common import create_error_json_obj, create_not_login_json_response, create_success_json_res_with
 from ..models.comment import Comment
 from ..models.device import Device
 from ..models.user import User
