6af8cbaa01ecbe7c88688afa1592367d0f0e4864
leonardodalinky <493987054@qq.com>
2020-09-09 17:19:11 +0800

    更改models/device.py中的错误

--- webservice/views/device.py
+++ webservice/views/device.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, JsonResponse
 
 from ..common.common import create_error_json_obj, create_success_json_res_with
 from ..models.device import Device
 from ..models.user import User
 
 
-def get_device_list(request: HttpRequest, **kwargs) -> JsonResponse :
+def get_device_list(request: HttpRequest, **kwargs) -> JsonResponse:
     owner_id = request.GET.get('owner_id')
     borrower_id = request.GET.get('borrower_id')
     if owner_id is None and borrower_id is None:
@@ -37,28 +37,31 @@ def get_device_list(request: HttpRequest, **kwargs) -> JsonResponse :
         return create_success_json_res_with({"devices": []})
     return create_success_json_res_with({"devices": list([device.toDict() for device in devices])})
 
-def get_device_id(request: HttpRequest, device_id, **kwargs) -> JsonResponse :
-    device = Device.objects.get(device_id=device_id)
-    if len(device) == 0:
-        return create_error_json_obj(400,'device not exsit')
-    return create_success_json_res_with({"device":device.toDict()})
 
-def patch_device_id(request: HttpRequest, device_id, **kwargs) -> JsonResponse :
+def get_device_id(request: HttpRequest, device_id, **kwargs) -> JsonResponse:
     device = Device.objects.get(device_id=device_id)
     if len(device) == 0:
-        return create_error_json_obj(400,'device not exsit')
+        return JsonResponse(create_error_json_obj(403, 'device not exsit'), status=400)
+    return create_success_json_res_with({"device": device.toDict()})
+
+
+def patch_device_id(request: HttpRequest, device_id, **kwargs) -> JsonResponse:
+    device = Device.objects.get(device_id=device_id)
+    if len(device) == 0:
+        return JsonResponse(create_error_json_obj(403, 'device not exsit'), status=400)
     name = request.POST.get('name')
     description = request.POST.get('description')
     if name != '':
-        device.name=name
-    if description !='':
-        device.description=description
+        device.name = name
+    if description != '':
+        device.description = description
     device.save()
     return create_success_json_res_with()
 
-def delete_device_id(request: HttpRequest, device_id, **kwargs) -> JsonResponse :
+
+def delete_device_id(request: HttpRequest, device_id, **kwargs) -> JsonResponse:
     device = Device.objects.get(device_id=device_id)
     if len(device) == 0:
-        return create_error_json_obj(400,'device not exsit')
+        return JsonResponse(create_error_json_obj(403, 'device not exsit'), status=400)
     device.delete()
     return create_success_json_res_with()
