3b2fd5b92c529ded279a58f37ac6bbccd39b138e
leonardodalinky <493987054@qq.com>
2020-09-10 17:40:58 +0800

    修改setting.py

--- DRP_Backend/settings.py
+++ DRP_Backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'er$uhr6bw-5@ic&9(n1pwhilcb6^20^p6dr4zp2=o6(q+ye5n1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ayajike.xyz']
 
